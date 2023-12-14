984622f223dc9feab3af8187e60cdb430aac5057
unknown <nikhilwaichal@yahoo.com>
2023-12-14 16:26:39 +0530

    commiting files

---
+++ basics.js
@@ -0,0 +1,20 @@
+//print in console
+console.log("Hello world !!")
+
+//
+
+/*
+*/
+
+
+//Variables
+//(let const) ES6 engine
+//Var ES5
+const a = 10
+let b = 20
+
+console.log(a+b)
+
+
+  
+
---
+++ dataTypes.js
@@ -0,0 +1,66 @@
+//Data types in Javascript 
+/*
+1.Number:Represents numeric values.
+Example:
+let num1 = 42;        // integer
+let num2 = 3.14;      // floating-point number
+let num3 = 2e5;       // scientific notation (200000)
+
+2.String:Represents sequences of characters.
+let str1 = "Hello, ";
+let str2 = 'world!';
+
+3.Boolean Represent Boolean value
+let isTrue = true;
+let isFalse = false;
+
+4.Undefined: Represents the absence of a value or uninitialized variable.
+Example:
+let undefinedVar;
+
+5.Null: Represents the intentional absence of any object value. Indicating the absence of an object or a placeholder for a non-existent value.
+Example:
+
+6.Bigint:Used for handling large number
+Example:
+let bigIntVar = 123456789012345678901234567890n;
+
+7.Object: Represents a collection of key-value pairs, or properties.
+Example:
+let person = {
+  name: 'John',
+  age: 30,
+  isStudent: false
+};
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+8.Symbol Represents a unique identifier & used for Creating unique property keys in objects, avoiding naming conflicts.
+Example:
+const myObject = {};
+const symbolKey = Symbol('uniqueKey');
+myObject[symbolKey] = 'some value';
+*/
+console.log(typeof(a))
+
+let uName="Nikhil"
+console.log(typeof(uName))
+
+let required=true
+console.log(typeof(required))
+
+let person = {
+    name: 'John',
+    age: 30,
+    isStudent: false
+  };
+
+console.log(typeof(person))
+
+let z = null
+console.log(typeof(z))
